dfa7903b61665787921c2b91c1006d3a55222144
mymeles <mymeles@ncsu.edu>
2026-05-20 14:12:29 -0400

    M18 fix-up: wire host auth into track module

--- apps/api/src/app/modules/tracks/track.module.ts
+++ apps/api/src/app/modules/tracks/track.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { GuestModule } from '../guests/guest.module';
 import { ModerationModule } from '../moderation/moderation.module';
 import { SessionModule } from '../sessions/session.module';
+import { SpotifyAuthModule } from '../spotify-auth/spotify-auth.module';
 import { SpotifyPlaybackModule } from '../spotify-playback/spotify-playback.module';
 import { SpotifySearchAdapter } from './spotify-search.adapter';
 import { TrackController } from './track.controller';
@@ -10,7 +11,7 @@ import { TrackRepository } from './track.repository';
 import { TrackSearchService } from './track-search.service';
 
 @Module({
-  imports: [GuestModule, SessionModule, SpotifyPlaybackModule, ModerationModule],
+  imports: [GuestModule, SessionModule, SpotifyAuthModule, SpotifyPlaybackModule, ModerationModule],
   controllers: [TrackController],
   providers: [SpotifySearchAdapter, TrackNormalizer, TrackRepository, TrackSearchService],
   exports: [TrackNormalizer, TrackRepository, TrackSearchService],
